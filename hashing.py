f160c2deb65485b3addc77a2d34e57ade989cd73
Anu C S <anucs2004@gmail.com>
2024-09-24 12:56:12 +0200

    update hashing.py

--- hashing.py
+++ hashing.py
@@ -1,62 +1,47 @@
-from flask import Flask, request
-import bcrypt
-# Expetions for sqlaclemy
-from sqlalchemy.exc import IntegrityError
+import bcrypt 
+from pymongo import MongoClient
 
-from dbmodel import User
-from db import db
+def connect_to_mongodb():
+    client = MongoClient('mongodb://localhost:27017/userDB')
+    db = client['userDB']
+    return db
 
-app = Flask(__name__)
-# SQLAlchemy config read more: https://flask-sqlalchemy.palletsprojects.com/en/2.x/
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
+def hash_password(password):
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password, salt
 
-@app.route('/register', methods=['POST'])
-def register():
-    try:
-        email = request.json.get('email', None)
-        password = request.json.get('password', None)
-        
-        if not email:
-            return 'Missing email', 400
-        if not password:
-            return 'Missing password', 400
-        
-        hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+def store_user(username, email, password):
+    db = connect_to_mongodb()
+    users_collection = db['users']
+    
+    hashed_password, salt = hash_password(password)
+    
+    user = {
+        'username': username,
+        'email': email,
+        'passwordHash': hashed_password,
+        'salt': salt
+    }
+    
+    result = users_collection.insert_one(user)
+    print(f'New user created with the following id: {result.inserted_id}')
 
-        user = User(email=email, hash=hashed)
-        db.session.add(user)
-        db.session.commit()
-
-        return f'Welcome! {email}', 200
-    except IntegrityError:
-        # the rollback func reverts the changes made to the db ( so if an error happens after we commited changes they will be reverted )
-        db.session.rollback()
-        return 'User Already Exists', 400
-    except AttributeError:
-        return 'Provide an Email and Password in JSON format in the request body', 400
-
-
-@app.route('/login', methods=['POST'])
-def login():
-    try:
-        email = request.json.get('email', None)
-        password = request.json.get('password', None)
-        
-        if not email:
-            return 'Missing email', 400
-        if not password:
-            return 'Missing password', 400
-        
-        user = User.query.filter_by(email=email).first()
-        if not user:
-            return 'User Not Found!', 404
-        
-
-        if bcrypt.checkpw(password.encode('utf-8'), user.hash):
-            return f'Logged in, Welcome {email}!', 200
-        else:
-            return 'Invalid Login Info!', 400
-    except AttributeError:
-        return 'Provide an Email and Password in JSON format in the request body', 400
+def verify_password(username, password):
+    db = connect_to_mongodb()
+    users_collection = db['users']
+    
+    user = users_collection.find_one({'username': username})
+    if not user:
+        print('User not found')
+        return False
+    
+    stored_hashed_password = user['passwordHash']
+    
+    is_match = bcrypt.checkpw(password.encode('utf-8'), stored_hashed_password)
+    if is_match:
+        print('Login successful')
+        return True
+    else:
+        print('Invalid password')
+        return False
